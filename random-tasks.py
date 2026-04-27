635d5885a055d4c3f7abbd7808dc97bf13a058aa
93Chidiebere <vincent.christopher.189736@unn.edu.ng>
2026-04-28 00:17:20 +0100

    Updated file

--- random-tasks.py
+++ random-tasks.py
@@ -22,13 +22,29 @@
 
 
 #.........................AREA OF A TRIANGLE.............................
-base = float(input("Enter the length of the base: "))
+# base = float(input("Enter the length of the base: "))
 
-height = float(input("Enter the height of the triangle: "))
+# height = float(input("Enter the height of the triangle: "))
 
-Area = 0.5 * base * height
+# Area = 0.5 * base * height
 
-print(f"Area of a Triangle = {Area}")
+# print(f"Area of a Triangle = {Area}")
 
 
 #............PYTHON PROGRAMME TO SWAP TWO VARIABLES.....................
+# a = input("Input value for a: ")
+# b = input("Input value for b: 5")
+
+# # Print value to display original
+# print(f"original values: a = {a}, b = {b}")
+
+# # Swap with temporary
+# temp = a
+# a = b
+# b = temp
+
+# print(f"swapped values are: a = {a}, b = {b}")
+
+#............. GENERATE RANDOM NUMBER.......................................
+import random
+print(f"Random number: {random.randint(10, 1000)}")
