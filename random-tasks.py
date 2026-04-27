d8b89f67228d7495b4cb57df1b3bebd5e02bd31a
93Chidiebere <vincent.christopher.189736@unn.edu.ng>
2026-04-28 00:03:58 +0100

    Added random tasks

---
+++ random-tasks.py
@@ -0,0 +1,34 @@
+# # Perform addition and division
+# .....................ADDITION................................
+# first_num = float(input("Enter first number: "))
+# sec_num = float(input("Enter second number: "))
+
+# addition = first_num + sec_num
+
+# print(f" sum: {first_num} + {sec_num} = {addition}")
+
+
+# .......................DIVISION............................
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+
+# if num2 == 0:
+#     print("division error, not allowed")
+# else:
+
+#     division = num1 / num2
+
+#     print(f" {num1} / {num2} = {division}")
+
+
+#.........................AREA OF A TRIANGLE.............................
+base = float(input("Enter the length of the base: "))
+
+height = float(input("Enter the height of the triangle: "))
+
+Area = 0.5 * base * height
+
+print(f"Area of a Triangle = {Area}")
+
+
+#............PYTHON PROGRAMME TO SWAP TWO VARIABLES.....................
